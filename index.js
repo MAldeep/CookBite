64a12e64a254eecf585f6b352ce63b66c6fe181f
MAldeep <m7mdeldeeeep@gmail.com>
2025-04-26 17:51:32 +0300

    26-4-2025

---
+++ index.js
@@ -0,0 +1,7 @@
+const side_bar = document.getElementById('sideBar');
+function showTheOffCanvas(){
+    side_bar.style.transform = `translateX(0)`
+}
+function closingTheOffCanvas(){
+        side_bar.style.transform = `translateX(110%)`
+}
